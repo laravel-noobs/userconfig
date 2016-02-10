e2c6dc0d921235963c3f16b33371056550348094
Kou Tsuneka <tsuneka.kou@gmail.com>
2016-02-10 19:04:25 +0700

    validator

--- src/DefinesConfigs.php
+++ src/DefinesConfigs.php
@@ -1,6 +1,10 @@
 <?php namespace KouTsuneka\UserConfig;
 
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\Request;
+use KouTsuneka\UserConfig\UserConfigFacade as UserConfig;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 trait DefinesConfigs
 {
@@ -40,11 +44,19 @@ trait DefinesConfigs
         $temp = $this->configs;
         foreach($path as $key)
         {
-            if(!isset($temp[$key]))
+            if(!array_has($temp, $key))
                 return null;
             $temp = $temp[$key];
         }
         return $temp;
+    }
+
+    protected function read_configs($names, $reload = false)
+    {
+        $ret = [];
+        foreach($names as $name)
+            $ret[str_replace('.', '_', $name)] = $this->read_config($name, $reload);
+        return $ret;
     }
 
     protected function write_config($name, $value, $save = true)
@@ -52,7 +64,7 @@ trait DefinesConfigs
         $path = explode('.', $name);
         $temp = &$this->configs;
         foreach($path as $key) {
-            if(!isset($temp[$key]))
+            if(!array_has($temp,$key))
                 return;
             $temp = &$temp[$key];
         }
@@ -66,6 +78,18 @@ trait DefinesConfigs
         $input = $request->input();
         if(!isset($input['name']) || !isset($input['value']))
             throw new BadRequestHttpException();
+
+        if($input['value'] === "NULL")
+            $input['value'] = null;
+
+        if(isset($this->configs_validate[$input['name']]))
+        {
+            $validator = Validator::make([$input['name'] => $input['value']],[
+                $input['name'] => $this->configs_validate[$input['name']]
+            ]);
+            if($validator->fails())
+                return Response::json($validator->errors()->all(), 400);
+        }
 
         $this->write_config($input['name'], $input['value']);
     }
